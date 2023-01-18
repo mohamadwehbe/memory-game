5233b437951fa948aaee1ffefc7b54320f7f1b76
mohamadwehbe <wehbe148@gmail.com>
2023-01-18 12:01:33 +0200

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Game from "../components/Game";
+import Header from "../components/Header";
+
+const Home = (): JSX.Element => {
+  return (
+    <>
+      <Header />
+      <Game />
+    </>
+  );
+};
+
+export default Home;
